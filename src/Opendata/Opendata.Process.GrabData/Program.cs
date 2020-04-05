6a308811427b7fc316d9c2cac3c64aa6ef1b25f5
Severin Anatoliy <severin@pointcad.ru>
2020-04-05 13:43:08 +0300

    update dependencies

---
+++ src/Opendata/Opendata.Process.GrabData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opendata.Process.GrabData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
